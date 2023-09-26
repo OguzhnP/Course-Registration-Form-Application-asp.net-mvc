3da51c5b5545fb2c70753972db9707d551d61aba
Oguzhan Portakal <101134423+OguzhnP@users.noreply.github.com>
2023-09-27 01:34:54 +0300

    .

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,34 @@
+﻿using KursBasvuru.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KursBasvuru.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm]Candidate candidate)
+        {
+            if (Repository.Applications.Any(c=> c.Email == candidate.Email))  
+                ModelState.AddModelError("", "There is already an application for you");
+           
+
+            if (ModelState.IsValid)
+            { 
+                Repository.Add(candidate);
+                return View("Feedback", candidate);
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/Candidate.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KursBasvuru.Models
+{
+    public class Candidate 
+    {
+        [Required(ErrorMessage ="Email is required")]
+        public string? Email { get; set; } 
+
+        [Required(ErrorMessage = "FirstName is required")] 
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")] 
+        public string? LastName { get; set; }
+
+        public string? FullName => $"{FirstName} {LastName?.ToUpper()}";
+
+        public int? Age { get; set; }
+
+        public string? SelectedCourse { get; set; }
+
+        public DateTime ApplyAt{ get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
